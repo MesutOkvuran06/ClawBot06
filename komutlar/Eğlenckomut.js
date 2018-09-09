82650c8bbe130f4e5ce929d23c928c0cd83eb9d3
MesutOkvuran06 <43097230+MesutOkvuran06@users.noreply.github.com>
2018-09-09 12:08:22 +0300

    Update Eğlenckomut.js

--- komutlar/Eğlenckomut.js
+++ komutlar/Eğlenckomut.js
@@ -8,7 +8,7 @@ exports.run = (client, message, params) => {
   .setTitle("Komutlar")
   .setDescription('')
   .setColor(0x00ffff)
-  .addField("**» Eğlence Komutları**", `!atatürk = Rastgele Atatürkün Fotoğraflarını Gösterir. \n!starwars = StarWars (Pixel Formatında) Filmini Gösterir. \n!banned = Dene ve Gör! \n!kahkaha = Kahkaha Atarsınız \n!herkesebendençay = Herkese Çay Alırsınız. \n!koş = Koşarsınız.\n!çayiç = Çay İçersiniz. \n!çekiç = İstediğiniz Kişiye Çekiç Atarsınız. \n!çayaşekerat = Çaya Şeker Atarsınız. \n!yumruk-at = Yumruk Atarsınız. \n!şanslısayım = Bot Sizin Şanslı Sayınızı Söyler. \n!espiriyap = Espiri yapar \n!yazı-tura = Yazı tura yapar \n!öp = Etiketlediğiniz kişiyi öper \n!balıktut = Balık tutar \n!sarıl = Etiketlediğiniz kişiye sarılır \n!sigara = Sigara içer`)
+  .addField("**» Eğlence Komutları**", `!atatürk = Rastgele Atatürkün Fotoğraflarını Gösterir. \n!starwars = StarWars (Pixel Formatında) Filmini Gösterir. \n!banned = Dene ve Gör! \n!kahkaha = Kahkaha Atarsınız \n!herkesebendençay = Herkese Çay Alırsınız. \n!koş = Koşarsınız.\n!çayiç = Çay İçersiniz. \n!çekiç = İstediğiniz Kişiye Çekiç Atarsınız. \n!çayaşekerat = Çaya Şeker Atarsınız. \n!yumruk-at = Yumruk Atarsınız. \n!şanslısayım = Bot Sizin Şanslı Sayınızı Söyler. \n!espiriyap = Espiri yapar \n!yazı-tura = Yazı tura yapar \n!öp = Etiketlediğiniz kişiyi öper \n!balıktut = Balık tutar \n!sarıl = Etiketlediğiniz kişiye sarılır \n!sigara = Sigara içer \n!nsfw = siz bilirsiniz:)`)
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
